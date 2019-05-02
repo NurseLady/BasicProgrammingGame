268fd2ad5e68d79d858948e0508eb5c7d9fe3e11
Svetlana Zaytseva <sveta-zay96@gmail.com>
2019-05-02 13:19:23 +0500

    GetObjectBodySize()
    Возвращает прямоугольник, описывающий местоположение объекта и его габариты

--- TheGame/GameObjectExtensions.cs
+++ TheGame/GameObjectExtensions.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.CompilerServices;
+
 namespace TheGame
 {
     public static class GameObjectExtensions
     {
          public static void Draw(this IGameObject gameObject, Game game, Graphics e)
          {
-             var r = gameObject.Size * 5;
-             Vector LeftTopCorner = new Vector(gameObject.Location.X - r, 
-                                               gameObject.Location.Y - r);
-            var rect = new Rectangle((int)LeftTopCorner.X,
-                (int)LeftTopCorner.Y, r * 2, r * 2);
+            var rect = GetObjectBodySize(gameObject);
             switch (gameObject)
             {
                 case SimpleEnemy _:
@@ -27,10 +25,18 @@ namespace TheGame
                     e.FillEllipse(Brushes.Maroon, rect);
                     break;
             }
-            var v = new Vector(1, 0).Rotate(gameObject.Direction) * r ;
+            var v = new Vector(1, 0).Rotate(gameObject.Direction) * gameObject.Size * 5;
             v += gameObject.Location;
             e.FillEllipse(Brushes.Chartreuse,  (float)v.X - gameObject.Size, (float)v.Y - gameObject.Size, 
                 gameObject.Size * 2, gameObject.Size * 2);
-        }
+         }
+
+         public static Rectangle GetObjectBodySize(this IGameObject gameObject)
+         {
+             var r = gameObject.Size * 5;
+             var leftTopCorner = new Vector(gameObject.Location.X - r, 
+                 gameObject.Location.Y - r);
+             return new Rectangle((int)leftTopCorner.X, (int)leftTopCorner.Y, r * 2, r * 2);
+         }
     }
 }
