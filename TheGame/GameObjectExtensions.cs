868bd5510973ef9842993919c597b176e5cb6936
Svetlana Zaytseva <sveta-zay96@gmail.com>
2019-05-02 13:30:27 +0500

    GetObjectRadius()

--- TheGame/GameObjectExtensions.cs
+++ TheGame/GameObjectExtensions.cs
@@ -7,9 +7,10 @@ namespace TheGame
 {
     public static class GameObjectExtensions
     {
-         public static void Draw(this IGameObject gameObject, Game game, Graphics e)
-         {
-            var rect = GetObjectBodySize(gameObject);
+        public static void Draw(this IGameObject gameObject, Game game, Graphics e)
+        {
+            var r = GetObjectRadius(gameObject);
+            var rect = GetObjectBody(gameObject);
             switch (gameObject)
             {
                 case SimpleEnemy _:
@@ -28,15 +29,20 @@ namespace TheGame
             var v = new Vector(1, 0).Rotate(gameObject.Direction) * gameObject.Size * 5;
             v += gameObject.Location;
             e.FillEllipse(Brushes.Chartreuse,  (float)v.X - gameObject.Size, (float)v.Y - gameObject.Size, 
-                gameObject.Size * 2, gameObject.Size * 2);
-         }
+            gameObject.Size * 2, gameObject.Size * 2);
+        }
 
-         public static Rectangle GetObjectBodySize(this IGameObject gameObject)
-         {
-             var r = gameObject.Size * 5;
-             var leftTopCorner = new Vector(gameObject.Location.X - r, 
-                 gameObject.Location.Y - r);
-             return new Rectangle((int)leftTopCorner.X, (int)leftTopCorner.Y, r * 2, r * 2);
-         }
+        public static Rectangle GetObjectBody(this IGameObject gameObject)
+        {
+            var r = GetObjectRadius(gameObject);
+            var leftTopCorner = new Vector(gameObject.Location.X - r, 
+             gameObject.Location.Y - r);
+            return new Rectangle((int)leftTopCorner.X, (int)leftTopCorner.Y, r * 2, r * 2);
+        }
+
+        public static int GetObjectRadius(this IGameObject gameObject)
+        {
+            return gameObject.Size * 5;
+        }
     }
 }
