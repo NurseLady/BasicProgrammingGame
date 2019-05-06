25cefff301d259fd467de5fbc4d86d806bed2d42
Svetlana Zaytseva <sveta-zay96@gmail.com>
2019-05-07 00:37:57 +0500

    Бонус к здоровью

---
+++ TheGame/HealthBonus.cs
@@ -0,0 +1,37 @@
+using System.Drawing;
+
+namespace TheGame
+{
+    public class HealthBonus : IBonus
+        {
+            public Vector Location { get; set; }
+            public double Direction { get; }
+            public float Size { get; }
+            public int Speed { get; }
+            public double SpeedFactor { get; }
+            public int Health { get; set; }
+            public bool IsAlive { get; private set; }
+            public int Costs { get; }
+            public Color MainСolor { get; } = ColorTranslator.FromHtml("#ff9578");
+
+            public HealthBonus(Vector location, int costs)
+            {
+                Location = location;
+                Size = 1;
+                Costs = costs;
+                Speed = 0;
+                IsAlive = true;
+            }
+        
+            public void UpdateDirection(){}
+
+            public void Kill() => IsAlive = false;
+
+            public void Use(Game game)
+            {
+                game.Player.Health += Costs;
+                Kill();
+            }
+        }
+    }
+}
