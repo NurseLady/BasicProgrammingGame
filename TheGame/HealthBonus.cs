9ed323b3318f83f4505467fe4a643a881d21b082
Svetlana Zaytseva <sveta-zay96@gmail.com>
2019-05-07 00:39:45 +0500

    Update HealthBonus.cs

--- TheGame/HealthBonus.cs
+++ TheGame/HealthBonus.cs
@@ -3,35 +3,34 @@ using System.Drawing;
 namespace TheGame
 {
     public class HealthBonus : IBonus
+    {
+        public Vector Location { get; set; }
+        public double Direction { get; }
+        public float Size { get; }
+        public int Speed { get; }
+        public double SpeedFactor { get; }
+        public int Health { get; set; }
+        public bool IsAlive { get; private set; }
+        public int Costs { get; }
+        public Color MainСolor { get; } = ColorTranslator.FromHtml("#ff9578");
+
+        public HealthBonus(Vector location, int costs)
         {
-            public Vector Location { get; set; }
-            public double Direction { get; }
-            public float Size { get; }
-            public int Speed { get; }
-            public double SpeedFactor { get; }
-            public int Health { get; set; }
-            public bool IsAlive { get; private set; }
-            public int Costs { get; }
-            public Color MainСolor { get; } = ColorTranslator.FromHtml("#ff9578");
+            Location = location;
+            Size = 1;
+            Costs = costs;
+            Speed = 0;
+            IsAlive = true;
+        }
+    
+        public void UpdateDirection(){}
 
-            public HealthBonus(Vector location, int costs)
-            {
-                Location = location;
-                Size = 1;
-                Costs = costs;
-                Speed = 0;
-                IsAlive = true;
-            }
-        
-            public void UpdateDirection(){}
+        public void Kill() => IsAlive = false;
 
-            public void Kill() => IsAlive = false;
-
-            public void Use(Game game)
-            {
-                game.Player.Health += Costs;
-                Kill();
-            }
+        public void Use(Game game)
+        {
+            game.Player.Health += Costs;
+            Kill();
         }
     }
 }
